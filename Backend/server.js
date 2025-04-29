1aa44d4f65521c5c192d1abef61cb5b0a1833050
Nishant8782 <nishantsharma8782@gmail.com>
2025-04-29 07:20:55 +0530

    Fix error in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -35,7 +35,7 @@ app.use("/api/v1/message", messageRoute);
 
 app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
 
